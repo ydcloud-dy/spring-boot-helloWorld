fecc8c287a402a63e88ea87db93a3e79c8037b7d
iKubernetes <mage@magedu.com>
2022-12-08 17:24:30 +0800

    add rollouts

--- src/main/java/com/neo/controller/HelloWorldController.java
+++ src/main/java/com/neo/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello Spring Boot 2.0!\n";
+        return "Hello Spring Boot 2.0!";
     }
 	
     @RequestMapping("/hello")
@@ -18,6 +18,6 @@ public class HelloWorldController {
 	
     @RequestMapping("/version")
     public String version() {
-        return "Spring Boot Helloworld, version 0.9.6.\n";
+        return "version 0.9.6!";
     }
 }
--- src/test/java/com/neo/controller/HelloTests.java
+++ src/test/java/com/neo/controller/HelloTests.java
@@ -35,4 +35,5 @@ public class HelloTests {
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Hello World from MageEdu.com!")));
     }
+
 }
